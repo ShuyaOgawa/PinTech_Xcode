8b2215b606be169ee876dc3b4b8f27626d981c78
小川秀哉 <shuya464924@gmail.com>
2017-10-09 10:33:31 +0900

    Initial Commit

---
+++ PinTech/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  PinTech
+//
+//  Created by 小川秀哉 on 2017/10/09.
+//  Copyright © 2017年 Digital Circus Inc. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
